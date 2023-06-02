13c56552d45a2104d87c673524ecbab58713c454
Juliocesargranzotti <jcgranzotti@gmail.com>
2023-06-02 15:51:04 -0300

    teste

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Cadastro } from '../cadastro';
+import { CadastroService } from '../cadastro.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  cadastro : Cadastro[] = [];
+  isEditing : boolean = false;
+  formGroupClient: FormGroup;
+  CadastroService: any;
+  submitted: boolean = false;
+
+
+  constructor (private cadastroService : CadastroService, private formBuilder : FormBuilder){
+
+    this.formGroupClient = formBuilder.group({
+      id : [''],
+      titulo : ['',[Validators.required]],
+      descricao : ['',[Validators.required]],
+      preco : ['',[Validators.required]],
+      date : ['',[Validators.required]],
+      status : ['',[Validators.required]]
+    });
+  }
 }
